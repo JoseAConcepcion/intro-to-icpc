1609ad2181c4c326e237f000302b738eced49218
Jose <concepcijoseantonio@gmail.com>
2025-04-14 22:56:21 -0400

    fix: boneca

--- cpp/boneca.cpp
+++ cpp/boneca.cpp
@@ -1,37 +1,47 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve()
 {
-    long long max = 0;
-    long long aux = 0;
     long long n;
     cin >> n;
     long long numbers[n];
-
-    for (int i =0; i < n; i++)
-    {
-        cin >> numbers[i];
-    }
+    long long count[30] = {0}; 
+    long long max = 0;
+    long long aux = 0;
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            aux += numbers[i] ^ numbers [j];
-        }
-        if (aux > max)
-        {
-            max = aux;
-        }
-        aux = 0;
+        cin >> numbers[i];
     }
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 30; j++) {
+            if (numbers[i] & (1 << j)) {
+                count[j]++; 
+            } 
+        } 
+    } 
+
+    for (int k = 0; k < n; k++) {
+        long long  XORsum = 0;
+        long long a_k = numbers[k]; 
+
+        for (int i = 0; i < 30; ++i) {
+            if (!(a_k & (1 << i))) {
+                XORsum += count[i] * (1 << i);
+            } else {
+                XORsum += (n - count[i]) * (1 << i);
+            }
+        }
+        if (XORsum > max)
+        {
+            max = XORsum;
+        }
+    }
+
     cout << max << endl;
 }
-
-
 
 int main(){
     int t;
